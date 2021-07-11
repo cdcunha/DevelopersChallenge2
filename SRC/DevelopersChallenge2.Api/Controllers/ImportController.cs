6ba8b2c07fd79b09e1cfe5b6bb158e7dd89bb4be
Charlestown Dias da Cunha <charlestown.dias.da.cunha@autodesk.com>
2021-07-11 16:00:32 -0300

    Create solution and some basics projects

---
+++ SRC/DevelopersChallenge2.Api/Controllers/ImportController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopersChallenge2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult ReconcileBankStatement(List<string> ofxFiles)
+        {
+            if (ofxFiles == null || ofxFiles.Count < 2)
+            {
+                return BadRequest("At least 2 files are required to do the bank reconciliation");
+            }
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SRC/DevelopersChallenge2.Helper/OfxHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DevelopersChallenge2.Helper
+{
+    public class OfxHelper
+    {
+        public void OfxToDataSet(string ofxData)
+        {
+            Regex regex = new(@"<STMTTRN>[<\w\s\>\.\-\[\]\:]*<\/STMTTRN>");
+            MatchCollection matches = regex.Matches(ofxData);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                Console.WriteLine("'{0}' repeated at positions {1} and {2}",
+                                  groups["word"].Value,
+                                  groups[0].Index,
+                                  groups[1].Index);
+            }
+        }
+    }
+}
