4ae96e3d38e3f780fbe53cc4a4d8bdf8485f88d3
Charlestown Dias da Cunha <charlestown.dias.da.cunha@autodesk.com>
2021-07-11 23:12:21 -0300

    Create unit tests

---
+++ SRC/DevelopersChallenge2.Tests/Controllers/ImportControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopersChallenge2.Api.Controllers.Tests
+{
+    [TestClass()]
+    public class ImportControllerTests
+    {
+        [TestMethod()]
+        public void ReconcileBankStatementTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
